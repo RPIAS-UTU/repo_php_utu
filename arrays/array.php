58521708bc0fed3d306b0926dce79faa1906d558
Richard Pias <richard.pias.info@gmail.com>
2021-04-25 19:01:10 -0300

    arrays

--- arrays/array.php
+++ arrays/array.php
@@ -5,8 +5,12 @@ $planetas = array();
 $planetas[] = 'Marte';
 $planetas[] = 'Tierra';
 $planetas[] = 'Venus';
-unset($planetas[1]);
+
+// provocar error de unset
+// la única posibilidad seria eliminar el último elemento
+unset($planetas[0]);
 
 for($i=0;$i < count($planetas); $i++){
     echo "<br>" . $planetas[$i];
 }
+
