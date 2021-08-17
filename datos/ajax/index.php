12b082721163acf3d9e1552dfb73c1f545757fcd
Richard Pias <richard.pias.info@gmail.com>
2021-08-17 16:27:46 -0300

    Ajax

--- datos/ajax/index.php
+++ datos/ajax/index.php
@@ -6,6 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  <link href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css" rel="stylesheet">
+ 
+  
   <title>AJAX - </title>
 </head>
 
@@ -41,24 +44,50 @@
         <label for="txt_segundo_apellido" class="form-label">Segundo Apellido</label>
         <input type="text" class="form-control" id="txt_segundo_apellido" value="" required>
       </div>
+
+      <div class="col">
+        <label for="txt_fecha_nac" class="form-label">Fecha Nacimiento</label>
+        <input type="date" class="form-control" id="txt_fecha_nac" value="" required>
+      </div>
+
     </div>
+    <div class="row">  <div class="col">&nbsp;&nbsp; </div> </div>
 
     <div class="row">
       <div class="col-3">
         <button id="btn_agregar" class="btn btn-primary" type="submit">Agregar Persona</button>
       </div>
+      <div class="col-3">
+        <button id="btn_modificar" class="btn btn-primary" type="submit">Modificar Persona</button>
+      </div>
+
+      <div class="col-3">
+        <button id="btn_eliminar" class="btn btn-primary" type="submit">Eliminar Persona</button>
+      </div>
 
     </div>
 
+    <div class="row">  <div class="col">&nbsp;&nbsp; </div> </div>
+
     <div class="row">
       <div class="col">
-        <table id="tabla_personas_jquery" class="table"></table>
+        <table id="tabla_personas_jquery" 
+        class="table"></table>
       </div>
     </div>
 
     <div class="row">
       <div class="col">
-        <table id="tabla_personas_js" class="table"></table>
+        <table id="tabla_personas_DataTables" class="table">
+
+        <tr>
+    <th>Cédula</th>
+    <th>Primer Nombre</th>
+    <th>Segundo Nombre</th>
+    <th>Primer Apellido</th>
+    <th>Segundo Apellido</th>
+  </tr>
+        </table>
       </div>
     </div>
 
@@ -69,6 +98,8 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+
 <script src="ajax.js"></script>
 
 </html>
