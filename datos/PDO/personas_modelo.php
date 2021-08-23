2bb009ce2fa140e03b54e892f8d0b1fe36526dc5
Richard Pias <richard.pias.info@gmail.com>
2021-08-23 20:42:38 -0300

    Encapsulamiento

--- datos/PDO/personas_modelo.php
+++ datos/PDO/personas_modelo.php
@@ -6,20 +6,35 @@ class Personas_Model extends Conexion
 
     private $conexion;
 
+    private $cedula;
+
+    public function getCedula()
+    {
+        return $this->cedula;
+    }
+
+    public function setPrimerNombre($cedula)
+    {
+        $this->cedula = $cedula;
+    }
+
+
+  
+
     public function __construct()
     {
         $this->conexion = new Conexion();
     }
-    
-    public static function Listar_Personas_Ajax(){
+
+    public static function Listar_Personas_Ajax()
+    {
         $resultado = null;
         try {
-            
+
             $con = new Conexion();
             $consulta = "SELECT id_persona, cedula, primer_nombre, 
             segundo_nombre, primer_apellido, segundo_apellido, fecha_nac FROM persona";
             $resultado = $con->query($consulta)->fetchAll(PDO::FETCH_ASSOC);
-            
         } catch (Exception $e) {
             throw new Exception($e->getMessage());
         }
@@ -28,14 +43,14 @@ class Personas_Model extends Conexion
     }
 
 
-    public static function Listar_Persona_Por_Cedula_Ajax($id_persona){
+    public static function Listar_Persona_Por_Cedula_Ajax($id_persona)
+    {
         $resultado = null;
         try {
-            
+
             $con = new Conexion();
             $consulta = "SELECT * FROM persona WHERE id_persona = '$id_persona'";
             $resultado = $con->query($consulta)->fetchAll(PDO::FETCH_ASSOC);
-            
         } catch (Exception $e) {
             throw new Exception($e->getMessage());
         }
@@ -43,25 +58,26 @@ class Personas_Model extends Conexion
         return $resultado;
     }
 
-    public static function Listar_Personas(){
+    public static function Listar_Personas()
+    {
         try {
-            
-           //$obj_conexion = Conexion::getConexion();
+
+            //$obj_conexion = Conexion::getConexion();
             $con = new Conexion();
-        
+
             if (!$con) {
                 echo "<h3>No se ha podido conectar PHP - MySQL, verifique sus datos.</h3><hr><br>";
             } else {
                 echo "<br>==========================================================================";
                 echo "<h3> Ejecución de Consulta SQL para obtener datos </h3><hr><br>";
                 echo "<br>==========================================================================";
-                
-        
+
+
                 /* ejemplo de una consulta */
-        
+
                 $consulta = "SELECT * FROM persona";
                 $resultado = $con->query($consulta);
-        
+
                 if ($resultado->fetchColumn() > 0) {
                     echo "<table border='1' align='center'>
                         <tr bgcolor='#E6E6E6'>
@@ -71,9 +87,9 @@ class Personas_Model extends Conexion
                             <th>Primer Apellido</th>
                             <th>Segundo Apellido</th>
                         </tr>";
-        
+
                     foreach ($resultado as $fila) {
-        
+
                         echo "<tr>
                         <td>" . $fila["cedula"] . "</td>";
                         echo "<td>" . $fila["primer_nombre"] . "</td>";
@@ -81,7 +97,6 @@ class Personas_Model extends Conexion
                         echo "<td>" . $fila["primer_apellido"] . "</td>";
                         echo "<td>" . $fila["segundo_apellido"] . "</td></tr>";
                     }
-                    
                 } else {
                     echo "No hay Registros";
                 }
@@ -89,15 +104,15 @@ class Personas_Model extends Conexion
         } catch (PDOException $e) {
             echo 'Falló la conexión: ' . $e->getMessage();
         }
-
     }
-    
-    public static function Listar_Personas_SP(){
+
+    public static function Listar_Personas_SP()
+    {
         try {
-            
-           //$obj_conexion = Conexion::getConexion();
+
+            //$obj_conexion = Conexion::getConexion();
             $con = new Conexion();
-        
+
             if (!$con) {
 
                 echo "<h3>No se ha podido conectar PHP - MySQL, verifique sus datos.</h3><hr><br>";
@@ -105,39 +120,36 @@ class Personas_Model extends Conexion
                 echo "<br>==========================================================================";
                 echo "<h3> Ejecución de un Procedimiento Almacenado para obtener datos </h3><hr><br>";
                 echo "<br>==========================================================================";
-                
-        
+
+
                 /* ejemplo de una consulta */
-        
+
                 $consulta = "CALL sp_obtener_personas()";
                 $resultado = $con->query($consulta);
-        
+
                 if ($resultado->fetchColumn() > 0) {
                     echo "<table border='1' align='center'>
                         <tr bgcolor='#E6E6E6'>
                             <th>Cédula</th>
                             <th>Nombre Completo</th>
                         </tr>";
-        
+
                     foreach ($resultado as $fila) {
-        
+
                         echo "<tr>
                         <td>" . $fila["cedula"] . "</td>";
                         echo "<td>" . $fila["nombre"] . "</td>";
                     }
-                    
                 } else {
                     echo "No hay Registros";
                 }
             }
-            
         } catch (PDOException $e) {
             echo 'Falló la conexión: ' . $e->getMessage();
         }
 
         echo "<br>===============================================================";
         echo "<br>===============================================================";
-
     }
 
     public static function Agregar_Persona_SP($cedula, $n1, $n2, $a1, $a2, $fnac)
@@ -146,13 +158,13 @@ class Personas_Model extends Conexion
             $con = new Conexion();
             $sql = "CALL sp_agregar_persona(?,?,?,?,?,?,?)";
             $insert =  $con->prepare($sql);
-            $insert->bindParam(1, $cedula, PDO::PARAM_INT); 
-            $insert->bindParam(2, $n1, PDO::PARAM_STR, 50); 
-            $insert->bindParam(3, $n2, PDO::PARAM_STR, 50); 
-            $insert->bindParam(4, $a1, PDO::PARAM_STR, 50); 
-            $insert->bindParam(5, $a2, PDO::PARAM_STR, 50); 
-            $insert->bindParam(6, $fnac, PDO::PARAM_STR, 10); 
-            $insert->bindParam(7, $idInsert, PDO::PARAM_INT|PDO::PARAM_INPUT_OUTPUT); 
+            $insert->bindParam(1, $cedula, PDO::PARAM_INT);
+            $insert->bindParam(2, $n1, PDO::PARAM_STR, 50);
+            $insert->bindParam(3, $n2, PDO::PARAM_STR, 50);
+            $insert->bindParam(4, $a1, PDO::PARAM_STR, 50);
+            $insert->bindParam(5, $a2, PDO::PARAM_STR, 50);
+            $insert->bindParam(6, $fnac, PDO::PARAM_STR, 10);
+            $insert->bindParam(7, $idInsert, PDO::PARAM_INT | PDO::PARAM_INPUT_OUTPUT);
             $insert->execute();
             return $idInsert;
         } catch (PDOException $e) {
@@ -216,9 +228,8 @@ class Personas_Model extends Conexion
         $update->bindParam(':a2', $a2, PDO::PARAM_STR, 25);
         $update->bindParam(':fnac', $fnac, PDO::PARAM_STR, 25);
         $update->bindParam(':id_persona', $id_persona, PDO::PARAM_INT);
-            
-        return $update->execute();
 
+        return $update->execute();
     }
 
     public function Modificar_Persona($cedula, $n1, $n2, $a1, $a2, $fnac)
